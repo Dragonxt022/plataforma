96c5bc0621278ec7be0461676e0b41c3214b3f32
Bruno Da Silva  Pissinatti <pissinatti2019@gmail.com>
2024-03-05 17:46:01 -0400

    criamos o migrete de inscrições

---
+++ app/Models/Inscricoes.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Inscricoes extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'quantidade_inscritos',
+        'valor_curso',
+        'subtotal',
+        'desconto',
+        'total',
+        'id_empresa',
+        'pdf_caminho',
+        'nome_treinamento',
+        'id_treinamento',
+        'nome_empresa',
+        'data_inicio',
+        'nome_juridico',
+        'cnpj',
+        'cep',
+        'cidade',
+        'bairro',
+        'rua',
+        'numero',
+        'responsavel',
+        'telefone',
+        'email',
+        'data_realizacao',
+        'status',
+        'data_termino',
+    ];
+}
---
+++ database/migrations/2024_03_05_201438_create_inscricoes_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('inscricoes', function (Blueprint $table) {
+        $table->id();
+        $table->string('quantidade_inscritos');
+        $table->decimal('valor_curso', 10, 2);
+        $table->decimal('subtotal', 10, 2);
+        $table->decimal('desconto', 10, 2);
+        $table->decimal('total', 10, 2);
+        $table->string('id_empresa');
+        $table->string('pdf_caminho')->nullable();
+        $table->string('nome_treinamento');
+        $table->string('id_treinamento');
+        $table->string('nome_empresa');
+        $table->date('data_inicio');
+        $table->string('nome_juridico');
+        $table->string('cnpj');
+        $table->string('cep');
+        $table->string('cidade');
+        $table->string('bairro');
+        $table->string('rua');
+        $table->string('numero');
+        $table->string('responsavel');
+        $table->string('telefone');
+        $table->string('email');
+        $table->date('data_realizacao');
+        $table->enum('status', ['Processando', 'Pago', 'Cancelado'])->default('Processando');
+        $table->date('data_termino');
+        $table->timestamps();
+
+        $table->foreign('id_empresa')->references('id')->on('empresas');
+        $table->foreign('id_treinamento')->references('id')->on('treinamentos');
+    });
+}
+
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inscricoes');
+    }
+};
